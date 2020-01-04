c20b37095261bc537614ffde42e8d7b7bb711f2b
jexists <jexists486@gmail.com>
2020-01-04 12:05:56 +0900

    naver keyword and yhnew on 200104

--- js/src/temp/date.js
+++ js/src/temp/date.js
@@ -10,7 +10,7 @@ function getTime() {
 	let nowH = date.getHours();
 	let nowMin = date.getMinutes();
 
-	now.innerText = `${nowY}.${myMonth[nowM]}.${nowD < 10 ? `0${nowD}` : nowD} ${nowH < 10 ? `0${nowH}` : nowH}:${nowMin < 10 ? `0${nowMin}` : nowMin}기준`;
+	now.innerText = `${nowY}.${myMonth[nowM]}.${nowD < 10 ? `0${nowD}` : nowD} ${nowH < 10 ? `0${nowH}` : nowH}:${nowMin < 10 ? `0${nowMin}` : nowMin} 기준`;
 }
 function init() {
 	getTime();
