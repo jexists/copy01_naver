4e4bd8a0e8de6f583d5e39e62e2d1e8dd2c5a8ab
jexists <jexists486@gmail.com>
2020-01-28 15:40:51 +0900

    200128

--- js/src/loadfile.js
+++ js/src/loadfile.js
@@ -5,16 +5,17 @@
 	const wrap = $('#wrap');
 
 	const keywordPopup = $('.keypopup');
-
+	const catnews = $('.catnews');
 
 	const JsLink = (temp) => {
 		return body.append(`<script src="../js/src/temp/${temp}.js"></script>`);
 	};
 
 	keywordPopup.load('./temp/keypopup.html',function(){
-		JsLink('date');
-		JsLink('keypopup');
+		// JsLink('date');
+		// JsLink('keypopup');
 	});
 
+	catnews.load('./temp/catnews.html');
 
 })(jQuery);
